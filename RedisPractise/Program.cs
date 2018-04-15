4826c9ab892bae4bfd0e8bd134d4550399b7344d
Joseph <lai225joseph@gmail.com>
2018-04-15 19:01:13 +0800

    redis

---
+++ RedisPractise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace RedisPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var internetPort = ConfigurationManager.AppSettings["internetPort"];
+            ConnectionMultiplexer conn = ConnectionMultiplexer.Connect(internetPort);
+
+            IDatabase cache = conn.GetDatabase();
+
+            cache.StringSet("key1","value1");
+            string data = cache.StringGet("key1");
+
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+}
